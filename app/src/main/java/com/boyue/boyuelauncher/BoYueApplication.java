bfd00ea6ba854d2fb3c8432554cad5c740a6491f
tianluhua <466469918@qq.com>
2018-05-11 22:30:51 +0800

    first commit

---
+++ app/src/main/java/com/boyue/boyuelauncher/BoYueApplication.java
@@ -0,0 +1,10 @@
+package com.boyue.boyuelauncher;
+
+import android.app.Application;
+
+/**
+ * Created by tianluhua on 2018/5/11.
+ */
+
+public class BoYueApplication extends Application {
+}
---
+++ app/src/main/java/com/boyue/boyuelauncher/MainActivity.java
@@ -0,0 +1,13 @@
+package com.boyue.boyuelauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
